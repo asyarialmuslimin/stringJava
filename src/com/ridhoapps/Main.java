a6e929b2c9a4b286f45368e6869a042a5d12fa01
asyarialmuslimin <ridhoahmadsaifur@outlook.com>
2017-10-23 10:40:30 +0700

    Revisi

--- src/com/ridhoapps/Main.java
+++ src/com/ridhoapps/Main.java
@@ -8,6 +8,8 @@ public class Main {
     public static void main(String[] args) {
 
         boolean quit = false;
+        boolean invalid = false;
+        int check;
         String nim;
 
         do {
@@ -17,64 +19,78 @@ public class Main {
 
                 if (nim.length() == 9) {
                     String fakultas = nim.substring(0, 2);
-                    switch (fakultas) {
-                        case "11":
-                            fakultas = "Teknik";
-                            break;
-                        case "12":
-                            fakultas = "Ekonomi";
-                            break;
-                        case "13":
-                            fakultas = "Hukum";
-                            break;
-                        case "14":
-                            fakultas = "Ilmu Kesehatan";
-                            break;
-                        default:
-                            fakultas = "undefined";
-                            break;
-                    }
                     String jurusan = nim.substring(2, 4);
-                    switch (jurusan) {
-                        case "31":
-                            jurusan = "Teknik Industri";
-                            break;
-                        case "32":
-                            jurusan = "Teknik Mesin";
-                            break;
-                        case "33":
-                            jurusan = "Teknik Komputer";
-                            break;
-                        case "34":
-                            jurusan = "Teknik Informatika";
-                            break;
-                        case "35":
-                            jurusan = "Manajemen";
-                            break;
-                        case "36":
-                            jurusan = "Akuntansi";
-                            break;
-                        case "37":
+                    check = Integer.parseInt(jurusan);
+                    if (fakultas == "11") {
+                        if (check > 30 && check < 35) {
+                            fakultas = "Teknik";
+                            switch (jurusan) {
+                                case "31":
+                                    jurusan = "Teknik Industri";
+                                    break;
+                                case "32":
+                                    jurusan = "Teknik Mesin";
+                                    break;
+                                case "33":
+                                    jurusan = "Teknik Komputer";
+                                    break;
+                                case "34":
+                                    jurusan = "Teknik Informatika";
+                                    break;
+                                default:
+                                    break;
+                            }
+                        } else {
+                            invalid = true;
+                        }
+                    } else if (fakultas == "12") {
+                        if (check == 35 || check == 36) {
+                            fakultas = "Ekonomi";
+                            switch (jurusan) {
+                                case "35":
+                                    jurusan = "Manajemen";
+                                    break;
+                                case "36":
+                                    jurusan = "Akuntansi";
+                                    break;
+                                default:
+                                    break;
+                            }
+                        } else {
+                            invalid = true;
+                        }
+                    } else if (fakultas == "13") {
+                        if (check == 37) {
+                            fakultas = "Hukum";
                             jurusan = "Ilmu Hukum";
-                            break;
-                        case "38":
+                        } else {
+                            invalid = true;
+                        }
+                    } else if (fakultas == "14") {
+                        if (check == 38) {
+                            fakultas = "Ilmu Kesehatan";
                             jurusan = "Ilmu Kesehatan";
-                            break;
-                        default:
-                            jurusan = "undefined";
-                            break;
+                        } else {
+                            invalid = true;
+                        }
+                    } else {
+                        invalid = true;
                     }
-                    String a = "20";
-                    String tahun = a.concat(nim.substring(4, 6));
-                    String absen = nim.substring(6, 9);
 
+                    if (invalid){
+                        JOptionPane.showMessageDialog(null, "NIM yang anda Masukkan Invalid, silahkan ulangi kembali");
+                    } else {
+                        String a = "20";
+                        String tahun = a.concat(nim.substring(4, 6));
+                        String absen = nim.substring(6, 9);
 
-                    JOptionPane.showMessageDialog(null, "NIM : " + nim + "\n" + "Tahun Angkatan : " + tahun + "\n" + "Fakultas : " + fakultas + "\n" + "Jurusan : " + jurusan + "\n" + "No. Absen : " + absen);
+                        JOptionPane.showMessageDialog(null, "NIM : " + nim + "\n" + "Tahun Angkatan : " + tahun + "\n" + "Fakultas : " + fakultas + "\n" + "Jurusan : " + jurusan + "\n" + "No. Absen : " + absen);
 
-                    int ulangi = JOptionPane.showOptionDialog(null, "Ingin Mengulangi Proses ?", "Ulangi Proses", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+                        int ulangi = JOptionPane.showOptionDialog(null, "Ingin Mengulangi Proses ?", "Ulangi Proses", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
 
-                    if (ulangi == 1)
-                        quit = true;
+                        if (ulangi == 1)
+                            quit = true;
+                    }
 
                 } else {
                     JOptionPane.showMessageDialog(null, "NIM yang anda Masukkan Invalid, silahkan ulangi kembali");
